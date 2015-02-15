50bf1007c72d13e5cce0c5be7c26f6c823d5e9d1
abma <spring@abma.de>
2015-02-15 01:14:05 +0100

    add modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+local modinfo = {
+  name = "Spring Menu",
+  shortName = "SM",
+  game = "Spring Menu",
+  shortGame = "SM",
+  mutator = "SM",
+  version = "$VERSION",
+  description =	"Spring default engine menu",
+  url =	"https://github.com/spring/menu-game.sdd",
+  modtype = 1,
+}
+
+return modinfo
+
