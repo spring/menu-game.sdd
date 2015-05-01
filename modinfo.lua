fd79384e699c532ddb672843709370a8c26d4d54
abma <spring@abma.de>
2015-05-01 19:04:01 +0200

    add missing depend

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,16 @@
 local modinfo = {
-  name = "Spring Menu",
-  shortName = "SM",
-  game = "Spring Menu",
-  shortGame = "SM",
-  mutator = "SM",
-  version = "$VERSION",
-  description =	"Spring default engine menu",
-  url =	"https://github.com/spring/menu-game.sdd",
-  modtype = 1,
+	name = "Spring Menu",
+	shortName = "SM",
+	game = "Spring Menu",
+	shortGame = "SM",
+	mutator = "SM",
+	version = "$VERSION",
+	description =	"Spring default engine menu",
+	url =	"https://github.com/spring/menu-game.sdd",
+	modtype = 1,
+	depend = {
+		"Spring Cursors",
+	}
 }
 
 return modinfo
